2321f2db98644d6446c2290be014fb1b5d5dc08f
Michael Jaros <michael.jaros@jarosoft.at>
2019-08-11 17:09:10 +0200

    Rename variable x to weekday

--- weekday.php
+++ weekday.php
@@ -29,26 +29,26 @@ if($m>=11) {
 
 $w = ($d + intval (2.6 * $m - 0.2) + $y  + intval ($y/4) + intval ($c/4) - 2*$c ) % 7;
 if($w == 1) {
-    $x = "Montag";
+    $weekDay = "Montag";
 } elseif($w == 2) {
-    $x = "Dienstag";
+    $weekDay = "Dienstag";
 } elseif($w == 3) {
-    $x = "Mittwoch";
+    $weekDay = "Mittwoch";
 } elseif($w == 4) {
-    $x = "Donnerstag";
+    $weekDay = "Donnerstag";
 } elseif($w == 5) {
-    $x = "Freitag";
+    $weekDay = "Freitag";
 } elseif($w == 6) {
-    $x = "Samstag";
+    $weekDay = "Samstag";
 } elseif($w == 0) {
-    $x = "Sonntag";
+    $weekDay = "Sonntag";
 } else {
     echo "Error: Unknown w={$w}\n";
     exit(1);
 }
 echo "Eingabe: {$day}.{$month}.{$year}\n";
 echo strftime("Berechnung PHP: Wochentag='%A'\n",strtotime("$year-$month-$day"));
-echo "Berechnung Algorithmus: Wochentag='{$x}'\n";
+echo "Berechnung Algorithmus: Wochentag='{$weekDay}'\n";
 if($argc>4 && ( $argv[4]=='-d' || $argv[4]=='--debug')) {
     echo "DEBUG: m={$m} y={$y} c={$c}\n";
 }
